db8b00a50fe315e0aa7df3ee7b612e96710c15e2
Daryl Duckmanton <daryl@lyraddigital.com>
2018-10-08 14:52:33 +1100

    Updating config to use Chromium

--- karma.conf.js
+++ karma.conf.js
@@ -4,6 +4,7 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
+    browsers: ['ChromeHeadless'],
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
