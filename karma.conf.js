5245cdeaf445a36536b7bb30399be18dd44440ef
Daryl Duckmanton <daryl@lyraddigital.com>
2018-10-08 15:25:27 +1100

    Adding log level to find out what's wrong with it.

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function (config) {
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
     autoWatch: true,
     browsers: ['Chrome', 'ChromiumHeadless'],
     singleRun: false
