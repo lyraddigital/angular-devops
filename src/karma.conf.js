f332b5333ce22268326783cd844d9829bd2729a1
Daryl Duckmanton <daryl@lyraddigital.com>
2018-10-10 16:14:56 +1100

    Trying other karma config file. I seemed to be using the wrong one.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -20,6 +20,17 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          // Without a remote debugging port, Google Chrome exits immediately.
+          '--remote-debugging-port=9222',
+        ],
+      }
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
